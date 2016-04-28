62d5f98f40fe7a7805f2fce9e9eeb436c32fa375
alexzxz <wu.allen154@gmail.com>
2016-04-28 03:30:20 -0400

    change welcome

--- public/js/controllers/Home.js
+++ public/js/controllers/Home.js
@@ -4,10 +4,11 @@
 // if there is a user, find local events that are to user preferences
 
 function IndexCtrl($scope, $http) {
-	$scope.tagline = 'Welcome, Time to play!'; 
+	
 
 	$http.get('/api/events').success(function(data, status, headers, config){
 		$scope.events = data.events;
+    $scope.tagline = 'Welcome, ' + data.user.name + '. Time to play!'; 
 	});
 	
   // $http.get('/api/rec').
