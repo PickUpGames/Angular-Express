5a5556c92b2abbd8b10ee1f0a86ddd0b1a4b58bb
Tommy Fang <fangt@rpi.edu>
2016-04-28 09:43:45 -0400

    added function to index controller.

--- public/js/controllers/Home.js
+++ public/js/controllers/Home.js
@@ -9,6 +9,26 @@ function IndexCtrl($scope, $http) {
 	$http.get('/api/events').success(function(data, status, headers, config){
 		$scope.events = data.events;
     $scope.tagline = 'Welcome, ' + data.user.name + '. Time to play!'; 
+			if(data.user.tag.length == 0) {
+			//loop through events list
+			for (var i = 0; i < $scope.events.length; i ++){
+				//set current list of tags in event.
+				$scope.cur_tags = $scope.events[i].tag;
+				//console.log($scope.cur_tags);
+				//loop through tag identifiers in user  tag list
+				for (var j = 0; j < $scope.user.tag.length; j++){
+					$scope.tag = $scope.user.tag[j];
+					//console.log("users tag" + $scope.tag);
+					//check if user tag is in event tag list.
+					if ($scope.cur_tags.indexOf($scope.tag) > -1){
+						//if true push to eventList
+						$scope.eventList.push($scope.events[i]);
+					} else {
+						//false
+					}
+				}
+		}
+		}
 	});
 	
   // $http.get('/api/rec').
